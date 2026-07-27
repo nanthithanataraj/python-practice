0f75c29effd688fa871ab6cf31490f75e29533dc
Nanthitha Natarajan <nanthithanataraj@gmail.com>
2026-07-27 17:49:24 +0530

    Added comments

--- python_q1_personal_intro.py
+++ python_q1_personal_intro.py
@@ -1,3 +1,10 @@
 name = input("Name :")
 age = int(input("age: "))
 print(f"My name is {name} and I am {age} years old.")
+
+
+#variable - named memory location used to store data and can to used to change the stored data during program execution.
+
+#input() always returns a string
+
+#(f"My name is {name} and I am {age} years old.") -> f-string -> formatted string literal.
