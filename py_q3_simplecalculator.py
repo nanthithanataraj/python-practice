efb88c80b7faa8b35c416004356f2b135a950421
Nanthitha Natarajan <nanthithanataraj@gmail.com>
2026-07-27 23:14:08 +0530

    added simple calculator

---
+++ py_q3_simplecalculator.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter input1 : "))
+num2 = int(input("Enter input2 : "))
+total = num1 +num2
+difference = num1 - num2
+product = num1*num2
+quotient = num1/num2
+
+print(f"The total, difference, product, quotient of {num1} and {num2} is {total}, {difference}, {product}, {quotient}.")
